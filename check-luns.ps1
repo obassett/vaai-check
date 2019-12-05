2fb7abfb0c48030dc77efc708c47c67b524f3b3f
Oliver Bassett <oliver@bassett.net.nz>
2019-12-05 17:12:12 +0900

    had to load vmware powercli modules

--- check-luns.ps1
+++ check-luns.ps1
@@ -17,6 +17,8 @@ $ESXHosts = Get-VMHost
 # Loop through list of Hosts and Execute a Job to collect the LUN information. This is run multi-threaded so that we don't have to wait for completion to get next server.
 foreach($EsxHost in $ESXHosts){
   $arrayJobs += Start-Job -ScriptBlock {
+    # Import Power-cli Modules
+    if (!(Get-module | where {$_.Name -eq "VMware.VimAutomation.Core"})) {Import-Module VMware.VimAutomation.Core}
     Connect-VIServer $args[0]
     $esxcli = Get-VMHost $EsxHost | Get-EsxCli -V2
     $esxclioutput = $esxcli.storage.core.device.vaai.status.get.Invoke()
@@ -31,8 +33,8 @@ foreach($EsxHost in $ESXHosts){
 $complete = $false
 while (-not $complete) {
     $arrayJobsInProgress = $arrayJobs |
-        Where-Object { $_.State -match ‘running’ }
-    if (-not $arrayJobsInProgress) { “All Jobs Have Completed” ; $complete = $true }
+        Where-Object { $_.State -match 'running'}
+    if (-not $arrayJobsInProgress) { "All jobs have completed"; $complete = $true }
 }
 
 $output = "{0},{1},{2},{3},{4},{5},{6}`r`n" -f "Hostname", "Device Name", "VAAIPluginName", "ATSStatus", "CloneStatus", "DeleteStatus", "ZeroStatus"
